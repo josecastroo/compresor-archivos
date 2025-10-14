b95843e5828ea347cca3276ad833876c290e1e09
jose <castrooj02@gmail.com>
2025-10-14 16:08:40 -0600

    foundations

---
+++ CompresorArchivos/CompresorArchivos/HuffmanNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompresorArchivos
+{
+    public class HuffmanNode<T>
+    {
+        public T Element { get; set; }
+        public int Frequency { get; set; }
+        public HuffmanNode<T> Left { get; set; }
+        public HuffmanNode<T> Right { get; set; }
+
+        public HuffmanNode(T e, int freq)
+        {
+            Element = e;
+            Frequency = freq;
+            Left = Right = null;
+        }
+
+        public HuffmanNode(T e, int freq, HuffmanNode<T> left, HuffmanNode<T> right)
+        {
+            Element = e;
+            Frequency = freq;
+            Left = left;
+            Right = right;
+        }
+
+        public bool IsLeaf() => Left == null && Right == null;
+
+        public int CompareTo(HuffmanNode<T> node) => Frequency.CompareTo(node.Frequency);
+    }
+}
---
+++ CompresorArchivos/CompresorArchivos/HuffmanTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompresorArchivos
+{
+    public class HuffmanTree<T>
+    {
+        public HuffmanNode<T> Root;
+
+        public HuffmanTree() { Root = null; }
+
+        public List<T> PreOrder()
+        {
+            var temp = new List<T>();
+            PreOrder(Root, temp);
+            return temp;
+        }
+
+        private void PreOrder(HuffmanNode<T> act, List<T> list)
+        {
+            if (act != null)
+            {
+                list.Add(act.Element);
+                PreOrder(act.Left, list);
+                PreOrder(act.Right, list);
+            }
+        }
+    }
+}
